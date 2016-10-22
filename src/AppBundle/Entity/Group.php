bd1888e11f7fcd4a2988c8251e5b98e787ad9a37
Muhamad Alfan <muhamad.alfan01@gmail.com>
2016-10-22 17:19:02 +0700

    penyesuaian name template twig dengan generator, crud pertama group

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="groups")
+ */
+class Group
+{
+    /**
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var  int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=77)
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="code", type="string", length=3)
+     * @Assert\Length(min=2, minMessage="Kurang panjang coy", max=3, maxMessage="Kepanjangan datanya coy")
+     *
+     * @var string
+     */
+    private $code;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Group
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Group
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+}
